3ec1d902f0393f8d33824a42c5043b2d5de99542
Inna Matviienko <ineskamit@gmail.com>
2026-04-26 15:16:58 +0200

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,4 @@
-`use script`;
-// Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
-
-// Доповни код функції таким чином, що:
-
-// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
-// Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
-
+"use script";
 
 function formatMessage(message, maxLength) {
 
@@ -15,15 +8,6 @@ function formatMessage(message, maxLength) {
         return `${message.slice (0, maxLength)}...`
     }
 }
-
-// function formatMessage(message, maxLength) {
-//   const formMessage = message.length <= maxLength ? `${message}` : `${message.slice(0, maxLength)}...`;
-//   return formMessage;
-// }
-
-
-
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,4 @@
-`use script`;
-// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
-
-// Доповни код функції таким чином, що:
-
-// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
-// Якщо в рядку відсутні заборонені слова, функція повертає буль false
+"use script";
 
 function checkForSpam(message) {
     if (message.toLowerCase().includes('spam')) {
@@ -15,9 +9,6 @@ function checkForSpam(message) {
         return 'false'
     }
 }
-
-
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
